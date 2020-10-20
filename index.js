67eef6392d62e2c53ad1070de5e9914af76db9c2
alancphilpott <alanp1995@gmail.com>
2020-10-20 22:39:15 +0100

    Created user and post models and wrote a query to retrieve all posts from mongo

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
+// Dependencies
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
+
+// Relatives
+const Post = require("./models/Post");
 const { MONGODB } = require("./config");
 
 const typeDefs = gql`
+    type Post {
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
     type Query {
-        sayHi: String!
+        getPosts: [Post]
     }
 `;
 
 const resolvers = {
     Query: {
-        sayHi: () => "Hello World!"
+        async getPosts() {
+            try {
+                const posts = await Post.find();
+                return posts;
+            } catch (err) {
+                throw new Error(err);
+            }
+        }
     }
 };
 
