0ab84dfe0b73b2bb5eab498ff239e629204c6f36
alancphilpott <alanp1995@gmail.com>
2020-10-20 23:18:17 +0100

    refactor typeDefs & resolvers to their own respective folders. add nodemon dev dependency. added graphql mutation to enable user registration. add bcryptjs & jsonwebtoken dependencies. user password is hashed. a jwt is returned in the query response.

---
+++ graphql/typeDefs.js
@@ -0,0 +1,29 @@
+const { gql } = require("apollo-server");
+
+module.exports = gql`
+    type Post {
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
+    type User {
+        id: ID!
+        username: String!
+        email: String!
+        createdAt: String!
+        token: String!
+    }
+    input RegisterInput {
+        username: String!
+        password: String!
+        confirmPassword: String!
+        email: String!
+    }
+    type Query {
+        getPosts: [Post]
+    }
+    type Mutation {
+        register(registerInput: RegisterInput): User!
+    }
+`;
