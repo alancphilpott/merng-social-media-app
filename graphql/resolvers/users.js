636e85343c22ce8c59d43b21ef037b529fbb45d9
alancphilpott <alanp1995@gmail.com>
2020-10-20 23:25:42 +0100

    check if username already exists

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
 
 const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
@@ -8,13 +9,19 @@ module.exports = {
     Mutation: {
         async register(
             _,
-            { registerInput: { username, email, password, confirmPassword } },
-            context,
-            info
+            { registerInput: { username, email, password, confirmPassword } }
         ) {
             // TODO: Validate User Data (e.g. Empty Fields)
-            // TODO: Make sure user doesn't already exist.
-            // TODO: Hash password and create an auth token
+
+            // Check If User Exists
+            const user = await User.findOne({ username });
+            if (user)
+                throw new UserInputError("Username Is Taken", {
+                    errors: {
+                        username: "This Username Is Taken"
+                    }
+                });
+            // Hash User Password
             password = await bcrypt.hash(password, 12);
 
             const newUser = new User({
