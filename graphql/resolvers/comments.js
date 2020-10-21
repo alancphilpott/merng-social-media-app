61b07cd0fe5e4c0461c486e60950d438af0a4525
alancphilpott <alanp1995@gmail.com>
2020-10-22 00:46:47 +0100

    + comment and like types, updated the Post type. added 3 new mutations for iteracting with posts.

---
+++ graphql/resolvers/comments.js
@@ -0,0 +1,32 @@
+const { UserInputError } = require("apollo-server");
+
+const Post = require("../../models/Post");
+const checkAuth = require("../../util/check-auth");
+
+module.exports = {
+    Mutation: {
+        createComment: async (_, { postId, body }, context) => {
+            const { username } = checkAuth(context);
+            if (body.trim() === "") {
+                throw new UserInputError("Empty Comment", {
+                    errors: {
+                        body: "Comment Body Must Not Be Empty"
+                    }
+                });
+            }
+
+            const post = await Post.findById(postId);
+
+            if (post) {
+                post.comments.unshift({
+                    body,
+                    username,
+                    createdAt: new Date().toISOString()
+                });
+
+                await post.save();
+                return post;
+            } else throw new UserInputError("Post Not Found");
+        }
+    }
+};
