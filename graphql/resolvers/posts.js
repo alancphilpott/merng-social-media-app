60b527f339d1ba615a39085a89cf32a96fb72ae7
alancphilpott <alanp1995@gmail.com>
2020-10-21 14:34:33 +0100

    + getPost & createPost query/mutation logic

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,14 +1,44 @@
 const Post = require("../../models/Post");
+const checkAuth = require("../../util/check-auth");
 
 module.exports = {
     Query: {
         async getPosts() {
             try {
-                const posts = await Post.find();
+                const posts = await Post.find().sort({ createdAt: -1 });
                 return posts;
             } catch (err) {
                 throw new Error(err);
             }
+        },
+        async getPost(_, { postId }) {
+            try {
+                const post = await Post.findById(postId);
+
+                if (post) {
+                    return post;
+                } else {
+                    throw new Error("Post Not Found");
+                }
+            } catch (err) {
+                throw new Error("Caught An Error => " + err);
+            }
+        }
+    },
+    Mutation: {
+        async createPost(_, { body }, context) {
+            const user = checkAuth(context);
+
+            const newPost = new Post({
+                body,
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString()
+            });
+
+            const post = await newPost.save();
+
+            return post;
         }
     }
 };
