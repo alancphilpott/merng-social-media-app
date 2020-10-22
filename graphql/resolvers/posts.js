d68a1911966dc8a40ed4dad4617a0631c050062c
alancphilpott <alanp1995@gmail.com>
2020-10-23 00:13:15 +0100

    + add validattion for user input on post creation

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -2,6 +2,7 @@ const { AuthenticationError, UserInputError } = require("apollo-server");
 
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
+const { validateCreatePost } = require("../../util/validators");
 
 module.exports = {
     Query: {
@@ -30,6 +31,11 @@ module.exports = {
     Mutation: {
         createPost: async (_, { body }, context) => {
             const user = checkAuth(context);
+
+            const { valid, errors } = validateCreatePost(body);
+            if (!valid) {
+                throw new UserInputError("Errors", { errors });
+            }
 
             const newPost = new Post({
                 body,
