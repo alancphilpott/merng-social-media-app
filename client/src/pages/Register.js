b59468160035089457b8f16895ab6c49c18190d6
alancphilpott <alanp1995@gmail.com>
2020-10-24 02:05:24 +0100

    + basic template setup for the Home, Login and Register pages

---
+++ client/src/pages/Register.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+function Register() {
+    return (
+        <div>
+            <h1>Register</h1>
+        </div>
+    );
+}
+
+export default Register;
