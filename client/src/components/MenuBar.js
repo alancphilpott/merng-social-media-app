c69e915055c83201333634b41a0ed686936259c9
alancphilpott <alanp1995@gmail.com>
2020-10-24 02:16:32 +0100

    + hook up the menu items with links to their respective route

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 function MenuBar() {
-    const [activeItem, setActiveItem] = useState("");
+    const [activeItem, setActiveItem] = useState("home");
 
     const handleItemClick = (e, { name }) => setActiveItem(name);
 
@@ -12,22 +13,23 @@ function MenuBar() {
                 name="home"
                 active={activeItem === "home"}
                 onClick={handleItemClick}
-            ></Menu.Item>
-            <Menu.Item
-                name="messages"
-                active={activeItem === "messages"}
-                onClick={handleItemClick}
+                as={Link}
+                to="/"
             ></Menu.Item>
             <Menu.Menu position="right">
                 <Menu.Item
                     name="login"
                     active={activeItem === "login"}
                     onClick={handleItemClick}
+                    as={Link}
+                    to="/login"
                 ></Menu.Item>
                 <Menu.Item
                     name="register"
                     active={activeItem === "register"}
                     onClick={handleItemClick}
+                    as={Link}
+                    to="/register"
                 />
             </Menu.Menu>
         </Menu>
