cd4551012d869c2e3f005c2a820a87019c29d78c
alancphilpott <alanp1995@gmail.com>
2020-10-24 01:26:44 +0100

    Install semantic ui and react router dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 // import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <div>
-                <h1>Hello There!</h1>
-            </div>
-        </div>
+        <Router>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/login" component={Login}></Route>
+            <Route exact path="/register" component={Register}></Route>
+        </Router>
     );
 }
 
