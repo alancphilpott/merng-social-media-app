b7b330b657d7373436c627c331d673d3d7479db0
alancphilpott <alanp1995@gmail.com>
2020-10-24 02:10:11 +0100

    use menubar in app router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 // import logo from "./logo.svg";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import MenuBar from "./components/MenuBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
 function App() {
     return (
         <Router>
+            <MenuBar />
             <Route exact path="/" component={Home}></Route>
             <Route exact path="/login" component={Login}></Route>
             <Route exact path="/register" component={Register}></Route>
