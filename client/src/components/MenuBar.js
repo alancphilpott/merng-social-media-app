89df3c5a82f7e4cb9c8b79b267d4a1ea1f98ce30
alancphilpott <alanp1995@gmail.com>
2020-10-24 02:09:36 +0100

    + semantic-ui menu added, modified to be a function and require useState

---
+++ client/src/components/MenuBar.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Menu } from "semantic-ui-react";
+
+function MenuBar() {
+    const [activeItem, setActiveItem] = useState("");
+
+    const handleItemClick = (e, { name }) => setActiveItem(name);
+
+    return (
+        <Menu pointing secondary>
+            <Menu.Item
+                name="home"
+                active={activeItem === "home"}
+                onClick={handleItemClick}
+            ></Menu.Item>
+            <Menu.Item
+                name="messages"
+                active={activeItem === "messages"}
+                onClick={handleItemClick}
+            ></Menu.Item>
+            <Menu.Menu position="right">
+                <Menu.Item
+                    name="login"
+                    active={activeItem === "login"}
+                    onClick={handleItemClick}
+                ></Menu.Item>
+                <Menu.Item
+                    name="register"
+                    active={activeItem === "register"}
+                    onClick={handleItemClick}
+                />
+            </Menu.Menu>
+        </Menu>
+    );
+}
+
+export default MenuBar;
