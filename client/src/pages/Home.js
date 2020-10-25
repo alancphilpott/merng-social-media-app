1cb925122ed037bdf7116df9263e8ecd3dc19603
alancphilpott <alanp1995@gmail.com>
2020-10-25 01:04:34 +0000

    + css class to page title

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,13 +6,12 @@ import PostCard from "../components/PostCard";
 function Home() {
     const {
         loading,
-        _,
         data: { getPosts: posts }
     } = useQuery(FETCH_POSTS_QUERY);
 
     return (
         <Grid columns={3}>
-            <Grid.Row>
+            <Grid.Row className="page-title">
                 <h1>Recent Posts</h1>
             </Grid.Row>
             <Grid.Row>
