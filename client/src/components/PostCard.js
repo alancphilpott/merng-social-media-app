ad0754f724fce3e1f32024951aca71fa029637ad
alancphilpott <alanp1995@gmail.com>
2020-10-25 01:33:17 +0100

    + moment dependency + PostCard from semantic-ui + query to get posts and home returns a semantic-ui grid

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card, Image } from "semantic-ui-react";
+import moment from "moment";
+
+function PostCard({
+    post: { body, createdAt, id, username, likeCount, commentCount, likes }
+}) {
+    return (
+        <Card fluid>
+            <Card.Content>
+                <Image
+                    floated="right"
+                    size="mini"
+                    src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+                />
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta as={Link} to={`/posts/${id}`}>
+                    {moment(createdAt).fromNow(true)}
+                </Card.Meta>
+                <Card.Description>{body}</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <p>Buttons Here</p>
+                {/* <LikeButton user={user} post={{ id, likes, likeCount }} />
+                <MyPopup content="Comment on post">
+                    <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
+                        <Button color="blue" basic>
+                            <Icon name="comments" />
+                        </Button>
+                        <Label basic color="blue" pointing="left">
+                            {commentCount}
+                        </Label>
+                    </Button>
+                </MyPopup>
+                {user && user.username === username && (
+                    <DeleteButton postId={id} />
+                )} */}
+            </Card.Content>
+        </Card>
+    );
+}
+
+export default PostCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,55 @@
-import React, { Component } from "react";
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { Grid } from "semantic-ui-react";
+import PostCard from "../components/PostCard";
 
 function Home() {
+    const {
+        loading,
+        data: { getPosts: posts }
+    } = useQuery(FETCH_POSTS_QUERY);
+
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <Grid columns={3}>
+            <Grid.Row>
+                <h1>Recent Posts</h1>
+            </Grid.Row>
+            <Grid.Row>
+                {loading ? (
+                    <h1>Loading Posts...</h1>
+                ) : (
+                    posts &&
+                    posts.map((post) => (
+                        <Grid.Column key={post.id}>
+                            <PostCard post={post} />
+                        </Grid.Column>
+                    ))
+                )}
+            </Grid.Row>
+        </Grid>
     );
 }
+
+const FETCH_POSTS_QUERY = gql`
+    query {
+        getPosts {
+            id
+            body
+            createdAt
+            username
+            likeCount
+            likes {
+                username
+            }
+            commentCount
+            comments {
+                id
+                username
+                createdAt
+                body
+            }
+        }
+    }
+`;
 
 export default Home;
