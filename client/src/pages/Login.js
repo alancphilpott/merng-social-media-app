dc6a0722baa2357972c2f7bca63ace58fe28d24d
alancphilpott <alanp1995@gmail.com>
2020-10-26 23:26:27 +0000

    Loggin in and registering now sets the app auth context

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Form, Button } from "semantic-ui-react";
 import { gql, useMutation } from "@apollo/client";
 
+import { AuthContext } from "../context/auth";
 import { useForm } from "../util/hooks";
 
 function Login(props) {
+    const context = useContext(AuthContext);
+
     const [errors, setErrors] = useState({});
 
     const { onChange, onSubmit, values } = useForm(loginUserCallback, {
@@ -13,8 +16,8 @@ function Login(props) {
     });
 
     const [loginUser, { loading }] = useMutation(LOGIN_USER, {
-        update(_, result) {
-            console.log(result);
+        update(_, { data: { login: userData } }) {
+            context.login(userData);
             props.history.push("/");
         },
         onError(err) {
