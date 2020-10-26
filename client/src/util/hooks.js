4d93853acb5c8dd2b2ba9733177e9498ffefc1f0
alancphilpott <alanp1995@gmail.com>
2020-10-26 21:27:26 +0000

    + useForm function to manage form states

---
+++ client/src/util/hooks.js
@@ -0,0 +1,20 @@
+import { useState } from "React";
+
+export const useForm = (callback, initialState = {}) => {
+    const [values, setValues] = useState(initialState);
+
+    const onChange = (event) => {
+        setValues({ ...values, [event.target.name]: event.target.value });
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        callback();
+    };
+
+    return {
+        onChange,
+        onSubmit,
+        values
+    };
+};
