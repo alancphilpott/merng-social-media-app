10d49613abdb03680b3bd0d67c92a0c1d4055563
alancphilpott <alanp1995@gmail.com>
2020-10-27 00:35:55 +0000

    + new AuthRoute which stops logged in users from accessing the /login and /register routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 
 import { AuthProvider } from "./context/auth";
+import AuthRoute from "./util/AuthRoute";
 
 import MenuBar from "./components/MenuBar";
 import Home from "./pages/Home";
@@ -18,9 +19,9 @@ function App() {
             <Router>
                 <Container>
                     <MenuBar />
-                    <Route exact path="/" component={Home}></Route>
-                    <Route exact path="/login" component={Login}></Route>
-                    <Route exact path="/register" component={Register}></Route>
+                    <Route exact path="/" component={Home} />
+                    <AuthRoute exact path="/login" component={Login} />
+                    <AuthRoute exact path="/register" component={Register} />
                 </Container>
             </Router>
         </AuthProvider>
