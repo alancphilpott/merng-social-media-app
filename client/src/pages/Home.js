7d89127864e3822c46516bfb14986fd8465f49e0
alancphilpott <alanp1995@gmail.com>
2020-10-28 00:52:46 +0000

    + new file to house query used in multiple places + home updated to use imported fetch posts query

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React, { useContext } from "react";
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { Grid } from "semantic-ui-react";
 
 import { AuthContext } from "../context/auth";
 import PostCard from "../components/PostCard";
 import PostForm from "../components/PostForm";
+import { FETCH_POSTS_QUERY } from "../util/graphql";
 
 function Home() {
     const { loading, _, data } = useQuery(FETCH_POSTS_QUERY);
@@ -36,27 +37,5 @@ function Home() {
         </Grid>
     );
 }
-
-const FETCH_POSTS_QUERY = gql`
-    query {
-        getPosts {
-            id
-            body
-            createdAt
-            username
-            likeCount
-            likes {
-                username
-            }
-            commentCount
-            comments {
-                id
-                username
-                createdAt
-                body
-            }
-        }
-    }
-`;
 
 export default Home;
