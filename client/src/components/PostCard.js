4445cf16f2df5a597529f31eff0928f14b8e9c7e
alancphilpott <alanp1995@gmail.com>
2020-11-02 01:45:54 +0000

    + comment button nopw uses MyPopup

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { Button, Card, Image, Icon, Label, Popup } from "semantic-ui-react";
+import { Button, Card, Image, Icon, Label } from "semantic-ui-react";
 import moment from "moment";
 
 import { AuthContext } from "../context/auth";
 import LikeButton from "./LikeButton";
 import DeleteButton from "./DeleteButton";
+import MyPopup from "../util/MyPopup";
 
 function PostCard({
     post: { body, createdAt, id, username, likeCount, commentCount, likes }
@@ -29,25 +30,22 @@ function PostCard({
             <Card.Content extra>
                 <LikeButton user={user} post={{ id, likes, likeCount }} />
 
-                <Popup
-                    content="Comment On This Post"
-                    inverted
-                    trigger={
-                        <Button
-                            labelPosition="right"
-                            as={Link}
-                            to={`/posts/${id}`}
-                            style={{ marginLeft: 8 }}
-                        >
-                            <Button basic color="teal">
-                                <Icon name="comments" />
-                            </Button>
-                            <Label basic color="teal" pointing="left">
-                                {commentCount}
-                            </Label>
+                <MyPopup content="Comment On This Post">
+                    <Button
+                        labelPosition="right"
+                        as={Link}
+                        to={`/posts/${id}`}
+                        style={{ marginLeft: 8 }}
+                    >
+                        <Button basic color="teal">
+                            <Icon name="comments" />
                         </Button>
-                    }
-                />
+                        <Label basic color="teal" pointing="left">
+                            {commentCount}
+                        </Label>
+                    </Button>
+                </MyPopup>
+
                 {user && user.username === username && (
                     <DeleteButton postId={id} />
                 )}
